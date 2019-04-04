f4b5bb312afcb620658ef406b2dfd80f3246fd8c
Gustavo <gustavo.a.dev@gmail.com>
2019-04-04 15:52:24 -0600

    Initial Commit

---
+++ BulletList/BulletList.h
@@ -0,0 +1,19 @@
+//
+//  BulletList.h
+//  BulletList
+//
+//  Created by Gustavo Alvarez on 4/4/19.
+//  Copyright © 2019 Gustavo Alvarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BulletList.
+FOUNDATION_EXPORT double BulletListVersionNumber;
+
+//! Project version string for BulletList.
+FOUNDATION_EXPORT const unsigned char BulletListVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BulletList/PublicHeader.h>
+
+

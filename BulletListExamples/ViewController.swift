0b990f4a1eff14bbd13db082e02fc436e12db140
Gustavo <gustavo.a.dev@gmail.com>
2019-04-04 16:09:04 -0600

    Initial project setup

---
+++ BulletListExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BulletListExamples
+//
+//  Created by Gustavo Alvarez on 4/4/19.
+//  Copyright © 2019 Gustavo Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
